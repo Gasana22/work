eb2d6fc1fa2f24eb5ff3dc7b6d477c6ebe0b4236
Gasana22 <114236235+Gasana22@users.noreply.github.com>
2024-11-14 09:44:36 +0300

    Add files via upload

---
+++ func.js
@@ -0,0 +1,12 @@
+//function for adding, substituting,multiplying and division
+
+function numbers(num_3,num_4){
+    var sum=num_3+num_4;
+    var sub=num_3-num_4;
+    var multi=num_3*num_4;
+    var div=num_3/num_4;
+
+    console.log("sum:",sum,
+        "sub:",sub,"multi:",multi, "div:",div);
+}
+numbers(45,8)
